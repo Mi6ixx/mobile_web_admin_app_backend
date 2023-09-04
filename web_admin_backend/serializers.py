772902392e10c9fb6fb68ef5865d9a5a2d904c8b
Benjamin <kodiugos@gmail.com>
2023-09-03 22:27:09 -0700

    added custom validator

--- web_admin_backend/serializers.py
+++ web_admin_backend/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import Lodge
-
+from .validators import validate_image_size
 
 class LodgeSerializer(serializers.ModelSerializer):
     # Define a custom ImageField form field
-    images = serializers.ImageField(max_length=None, use_url=True)  # use_url=True to display image URLs
+    images = serializers.ImageField(max_length=None, use_url=True, validators=[validate_image_size])  # use_url=True to display image URLs
 
     class Meta:
         model = Lodge
