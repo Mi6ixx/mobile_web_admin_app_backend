c0b2cb973a4969bc42e3148799f3d72fe1b1111f
Benjamin <kodiugos@gmail.com>
2023-09-26 14:32:20 -1100

    created and registered student signals.py file

--- mobile_backend/apps.py
+++ mobile_backend/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MobileBackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mobile_backend'
+
+    def ready(self):
+        import mobile_backend.signals
---
+++ mobile_backend/signals.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from core.models import Student
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_student(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
