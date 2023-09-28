feb54eff987c677459f601b4eb89da98da0670c2
Benjamin <kodiugos@gmail.com>
2023-09-28 11:50:27 -1100

    created roommate-request model

--- core/models.py
+++ core/models.py
@@ -32,6 +32,7 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=11)
     user_type = models.CharField(max_length=20, choices=UserType.choices, blank=True, null=True)
     is_admin = models.BooleanField(default=False)
+    friends = models.ManyToManyField('self', blank=True)
 
     objects = CustomUserManager()
 
@@ -68,6 +69,30 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+
+class FriendRequest(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('declined', 'Declined'),
+    )
+
+    from_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='sent_requests',
+    )
+    to_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='received_requests',
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.from_user} -> {self.to_user}'
 
 
 class Lodge(models.Model):
