834e09e34cb4a7b99af4ecf3f6bbd6b22eed36d7
Benjamin <kodiugos@gmail.com>
2023-09-28 11:51:00 -1100

    implemented roommates-request API endpoints

--- mobile_backend/serializers.py
+++ mobile_backend/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Student, LodgeReview
+from core.models import Student, LodgeReview, FriendRequest
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -21,3 +21,12 @@ class StudentReviewSerializer(serializers.ModelSerializer):
         model = LodgeReview
         fields = ['id', 'rating', 'comment']
         read_only_fields = ['id']
+
+
+class FriendRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendRequest
+        fields = ['id', 'from_user', 'to_user', 'status']
+        read_only_fields = ['id']
+
+
