dc9e23a7cf82260ee168a2cdc14be5a96d7a76b4
Benjamin <kodiugos@gmail.com>
2023-09-29 07:23:17 -1100

    File changes

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import CustomUser, Student, Lodge
+from .models import CustomUser, Student, Lodge, LodgeAmenity, LodgeReview
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Student)
 admin.site.register(Lodge)
+admin.site.register(LodgeAmenity)
+admin.site.register(LodgeReview)
 
