b59a7b571a57664ac9cf0915d0747482ac53b3cb
Benjamin <kodiugos@gmail.com>
2023-09-29 06:59:57 -1100

    requirements for deployment on render

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -0 errexit
+
+pip install -r requirements.txt
+
+
+python manage.py collectstatic --no-input
+python manage.py migrate
--- roammates_backend/settings.py
+++ roammates_backend/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -172,6 +173,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LANGUAGE_CODE = 'en-us'
 
