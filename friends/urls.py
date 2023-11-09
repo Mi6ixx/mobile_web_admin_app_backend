e581911323865b719ca1961edea56236d307824a
USER <kodiugos@gmail.com>
2023-11-09 11:05:29 +0000

    changed url parameter

--- friends/urls.py
+++ friends/urls.py
@@ -3,5 +3,5 @@ from .views import SendFriendRequest, AcceptFriendRequest
 
 urlpatterns = [
     path('send-friend-request/<int:to_user_id>/', SendFriendRequest.as_view(), name='send-friend-request'),
-    path('accept-friend-request/<int:to_user_id>/', AcceptFriendRequest.as_view(), name='accept-friend-request'),
+    path('accept-friend-request/<int:friend_request_id>/', AcceptFriendRequest.as_view(), name='accept-friend-request'),
 ]
